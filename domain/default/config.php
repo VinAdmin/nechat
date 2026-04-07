9e59512afb8e2edab0649158ac086285669053d9
Vitaliy Olkhin <ovvitalik@gmail.com>
2026-04-07 12:59:34 +0500

    bootstrap-5.3.8

--- domain/default/config.php
+++ domain/default/config.php
@@ -1,10 +1,10 @@
 <?php
 \wco\kernel\WCO::setCss([
-    '/default/bootstrap-4.6.1/css/bootstrap.css',
+    '/default/bootstrap-5.3.8/css/bootstrap.css',
     '/default/css/style.css',
 ]);
 \wco\kernel\WCO::setJs([
     '/default/js/jquery-3.6.0.js',
-    '/default/bootstrap-4.6.1/js/bootstrap.js',
+    '/default/bootstrap-5.3.8/js/bootstrap.js',
     '/default/js/notify.js'
 ], 'end');
