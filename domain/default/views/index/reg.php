3bb022e32302c07aa8cc5b542229d3efa85fb066
Vitaliy Olkhin <ovvitalik@gmail.com>
2026-04-20 18:21:35 +0500

    Обновление страницы регистрации

--- domain/default/views/index/reg.php
+++ domain/default/views/index/reg.php
@@ -10,44 +10,58 @@ $form = new Form();
 <div class="container">
     <div class="row justify-content-md-center align-items-center">
         <div class="col-sm-11 col-lg-5">
-            <div id="notify"></div>
+            <div class="block-authorisation border-primary mb-3">
+                <div class="row g-0">
+                    <div class="col-sm-1 col-md-5">
+                        <img src="/default/images/logotip.png" class="card-img-top" alt="NeChat">
+                    </div>
+                    
+                    <div class="col-sm-11 col-md-7">
+                        <div class="card-body">
+                            <h1><?=$this->title?></h1>
+                            
+                            <div id="notify"></div>
             
-            <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="<?= WCO::Url('/')?>">Авторизация</a></li>
-                    <li class="breadcrumb-item active" aria-current="page"><?=$this->title?></li>
-                </ol>
-            </nav>
-            
-            <h1><?=$this->title?></h1>
-            
-            <?=$form->FormStart('reg','post',null,'off')?>
-                <div class="form-group">
-                    <label for="login">Логин *</label>
-                    <?=$form->Input('text','login', '', [
-                        'class'=>'form-control col-lg-12',
-                        'atr'=>'required',
-                        'placeholder'=>'Логин'
-                    ])->Field()?>
+                            <?=$form->FormStart('reg','post',null,'off')?>
+                                <div class="mb-3">
+                                    <label for="login">Логин *</label>
+                                    <?=$form->Input('text','login', '', [
+                                        'class'=>'form-control col-lg-12',
+                                        'atr'=>'required',
+                                        'placeholder'=>'Логин'
+                                    ])->Field()?>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Пароль *</label>
+                                    <?=$form->Input('password','password',null,[
+                                        'class'=>'form-control col-lg-12',
+                                        'atr'=>'required',
+                                    ])->Field()?>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Еще раз пароль *</label>
+                                    <?=$form->Input('password','re_password',null,[
+                                        'class'=>'form-control col-lg-12',
+                                        'atr'=>'required',
+                                    ])->Field()?>
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <?=$form->Input('submit','registr','Зарегистрироваться',[
+                                        'class'=>'btn btn-primary',
+                                    ])->Field()?>
+                                </div>
+                            <?=$form->FormEnd()?>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label>Пароль *</label>
-                    <?=$form->Input('password','password',null,[
-                        'class'=>'form-control col-lg-12',
-                        'atr'=>'required',
-                    ])->Field()?>
-                </div>
-                <div class="form-group">
-                    <label>Еще раз пароль *</label>
-                    <?=$form->Input('password','re_password',null,[
-                        'class'=>'form-control col-lg-12',
-                        'atr'=>'required',
-                    ])->Field()?>
-                </div>
-                    <?=$form->Input('submit','registr','Зарегистрироваться',[
-                        'class'=>'btn btn-primary',
-                    ])->Field()?>
-            <?=$form->FormEnd()?>
+                
+                <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?= WCO::Url('/')?>">Авторизация</a></li>
+                        <li class="breadcrumb-item active" aria-current="page"><?=$this->title?></li>
+                    </ol>
+                </nav>
+            </div>
         </div>
     </div>
 </div>
